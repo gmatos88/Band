6a74da395771fc472463e0f6d3929cabe3c1877a
bannerg <1625238@utp.edu.pe>
2018-02-12 10:14:11 -0500

    Reformat v1.0

---
+++ src/pe/edu/utp/bands/models/DTO/User.java
@@ -0,0 +1,88 @@
+package pe.edu.utp.bands.models.DTO;
+
+public class User {
+    //User DTO
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+
+    private String name;
+    private String lastname;
+
+    private int status;
+    private int type;
+
+
+    public User() {
+    }
+
+    public User(int id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+}
